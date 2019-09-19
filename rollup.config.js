602883d3b738b5c2e0a719d82421cb7695ba52cf
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-09-19 00:43:00 -0400

    Change imported name to match updated official docs - https://github.com/rollup/rollup-plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import json from "rollup-plugin-json";
-import resolve from "rollup-plugin-node-resolve";
+import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
@@ -35,6 +35,6 @@ module.exports = [
       format: "umd",
       name: "TRB" // Tape Rollup Boilerplate
     },
-    plugins: [resolve(), json(), ...babelPlugin, commonjs(), terser()]
+    plugins: [nodeResolve(), json(), ...babelPlugin, commonjs(), terser()]
   }
 ];
